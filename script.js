cd8d3e9ec0dd69a61866488a2f6771f6b7573a9a
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-11 21:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=shortHex.length-3,isCapital=false;
+	let ouput="#";
+	while(i<shortHex.length){
+		let asciicode=shortHex[i].charCodeAt(0);
+		if(asciicode>=65 && asciicode<=90){
+			isCapital=false;
+		}
+		output+=shortHex[i]+shortHex[i];
+	}
+	return isCapital?output.toUpperCase():output;
 };
 
-// Do not change the code below.
+ // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
